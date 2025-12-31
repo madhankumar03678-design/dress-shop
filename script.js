45889f32a01158fce277c13b9b6ce81b5d5c4a23
madhankumar03678-design <madhankumar03678@gmail.com>
2025-12-31 18:11:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let cart = 0;
+
+function addToCart() {
+  cart++;
+  alert("Added to cart 🛒");
+}
+
+function viewCart() {
+  alert("Items in cart: " + cart);
+}
+let cart = 0;
+
+function addToCart() {
+  cart++;
+  alert("Added to cart 🛒");
+}
+
+function buyNow() {
+  alert("Redirecting to payment 💳 (demo)");
+}
